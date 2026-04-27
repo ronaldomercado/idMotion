b9cd5f547eaed242ed47be099233e25a1cc79b88
Ronaldo Mercado <ronaldo.mercado@diamond.ac.uk>
2026-04-27 11:35:12 +0100

    builder updates

--- etc/builder.py
+++ etc/builder.py
@@ -1,2 +1,10 @@
+from iocbuilder import AutoSubstitution
+#, ModuleBase, records, Device, Xml
+from iocbuilder.hardware import Calc
 
+class IDGapSoftMotorTemplate(AutoSubstitution):
+    Dependencies = (Calc,)
+    TemplateFile = 'IDGapSoftMotor.template'
 
+class id4AxisGui(AutoSubstitution):
+    TemplateFile = 'ID_4VAxis_gui.template'
